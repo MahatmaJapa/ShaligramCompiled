2da1a182c9aa526b0604d667a5d1296fd0ed228c
Diego Montalvo <diego@hwdy.in>
2017-12-06 00:26:59 -0800

    First commit

---
+++ Documents/_projects/Clients/shaligram/shaligram/shaligram-master/build/release/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/downloads/cryptonote-master/src
+# Build directory: /home/ubuntu/downloads/cryptonote-master/build/release/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
